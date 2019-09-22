364322af398d2ed48431597cbb4f7bd15e724004
Jeung <lozente@gmail.com>
2019-09-22 17:46:32 -0400

    survey tab added

--- frontend/components/AppContainer.js
+++ frontend/components/AppContainer.js
@@ -7,7 +7,8 @@ import Header from './Header';
 import Land from './Land';
 import Issue from './Issue/Issue';
 import Login from './Login/Login';
-import AdminIssue from './AdminIssue/AdminIssues'
+import AdminIssue from './AdminIssue/AdminIssues';
+import Surveys from './Surveys/Surveys';
 import requireAuth from '../helpers/requireAuth';
 
 class AppContainer extends Component {
@@ -22,6 +23,7 @@ class AppContainer extends Component {
                 <Switch>
                   <Route exact path="/" component={Land} />
                   <Route path="/issues" component={Issue} />
+                  <Route path="/surveys" component={Surveys} />
                   {/*<Route path="/adminissues" component={requireAuth(AdminIssue)} />*/}
                   <Route path="/login" component={Login} />
                   <Route component={Land} />
