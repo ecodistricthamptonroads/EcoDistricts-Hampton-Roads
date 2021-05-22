91a1bfb0227cf96fd8e37ceee19e00d48d29aebb
ubersim <simin.li@protonmail.com>
2021-05-22 12:28:33 -0400

    changed server file from pre-strapi to strapi server.js

--- sqlite-backend/config/server.js
+++ sqlite-backend/config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://api.ecodistricthamptonroads.org',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '64ea995b492ccf64b54e3685bcc76023')
