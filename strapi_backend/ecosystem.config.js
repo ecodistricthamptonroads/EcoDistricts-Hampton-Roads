5b49b19e4c02ff9a2592141d12fe9d84fcd3394d
ubersim <simin.li@protonmail.com>
2021-05-29 07:53:53 -0400

    updating packages, deleted old backend

--- strapi_backend/ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [
-    {
-      name: "strapi",
-      script: "npm",
-      args: "start",
-    },
-  ],
-};
--- strapi_backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'cb5f093c-c9f7-4bf7-8392-ad1f8c470bb6'
-};
